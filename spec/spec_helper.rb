52411827f88d902c9bea060a226fdc3034c757df
gin0606 <kkgn06@gmail.com>
2016-06-27 01:40:17 +0900

    Use coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'markov_rensa'
 
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear!('rails')
+end
+
 MarkovRensa::Configuration.dictionary = 'spec/fixtures/ipadic'
